3dcae465bf8e21e74c0faf997992da70bc28e4a7
Walid Maarad <walsam@hotmail.com>
2020-11-22 20:23:14 +0100

    feature: roling the dice add the current score to the player, if he roles a 1, it gets reset to 0 and the turn change to the other one.

--- app.js
+++ app.js
@@ -33,6 +33,21 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. update the round score if the rolled number was not a 1
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-'+activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        activePlayer === 0 ? activePlayer=1 : activePlayer=0;
+        roundScore =0;
+        document.getElementById('current-0').textContent='0';
+        document.getElementById('current-1').textContent='0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        diceDOM.style.display = 'none';
+    }
 })
 
 
