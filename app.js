921944101b3c8c3fb9fb925bddf3b88b4554dccb
Walid Maarad <walsam@hotmail.com>
2020-11-22 20:56:55 +0100

    feature: adding the logic to new game with an init function.

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+inti();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
@@ -74,6 +66,24 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 }
 
-//document.querySelector('#current-'+activePlayer).textContent = dice;
-//document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+dice+'</em>';
-//document.querySelector('.dice').style.display = 'none';
+document.querySelector('.btn-new').addEventListener('click',inti);
+
+function inti() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
